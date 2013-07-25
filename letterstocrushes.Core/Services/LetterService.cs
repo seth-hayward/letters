c73bb7ab13d8aa94b0c38a57574cdb7f4d8a2de0
Seth Hayward <seth.hayward@gmail.com>
2013-07-25 15:45:49 -0700

    ban

--- letterstocrushes.Core/Services/LetterService.cs
+++ letterstocrushes.Core/Services/LetterService.cs
@@ -143,6 +143,7 @@ namespace letterstocrushes.Core.Services
                 banned_ips.Add("66.168.196.58");
                 banned_ips.Add("74.101.158.189");
                 banned_ips.Add("190.235.4.27");
+                banned_ips.Add("108.203.48.88");
                 banned_ips.Add("100.2.225.62"); // initials girl
                 banned_ips.Add("24.5.135.227"); // seth
                 // banned_ips.Add("74.101.102.68"); this person seems okay now
