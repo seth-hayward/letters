092aeb7653b4fde3eb5d8b660b4e09c100ebb3cd
Seth Hayward <seth.hayward@gmail.com>
2013-07-26 10:30:26 -0700

    adding block object with enums

---
+++ letterstocrushes.Core/Model/Block.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace letterstocrushes.Core.Model
+{
+
+    public enum blockType
+    {
+        blockIP = 1,
+        blockSubnet = 2,
+        blockGUID = 3
+    }
+
+    public enum blockWhat
+    {
+        blockLetter = 1,
+        blockComment = 2,
+        blockChat = 3
+    }
+
+    public class Block
+    {
+        public int Id { get; set; }
+        public Nullable<int> Type { get; set; }
+        public string Value { get; set; }
+        public Nullable<int> What { get; set; }
+    }
+}
