e34dca8705884a9261c75310ec45d43b05e916de
Seth Hayward <seth.hayward@gmail.com>
2013-07-26 12:02:07 -0700

    adding beginning block controller

---
+++ letterstocrushes.UI/Controllers/BlockController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace letterstocrushes.Controllers
+{
+    public class BlockController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
