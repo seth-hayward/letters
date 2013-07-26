aa40fec944d9afc6331331a01c75552097807e41
Seth Hayward <seth.hayward@gmail.com>
2013-07-26 10:30:48 -0700

    added interface for blocks

---
+++ letterstocrushes.Core/Interfaces/IQueryBlocks.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using letterstocrushes.Core.Model;
+
+namespace letterstocrushes.Core.Interfaces
+{
+    public interface IQueryBlocks
+    {
+        void Add(int type, string value, int what);
+        List<Block> getBlocks(int type, int what);
+    }
+}
