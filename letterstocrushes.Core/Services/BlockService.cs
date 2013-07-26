79d45aa90a3882a2c968226dbb59c4025a0fd9df
Seth Hayward <seth.hayward@gmail.com>
2013-07-26 11:56:10 -0700

    adding block service

---
+++ letterstocrushes.Core/Services/BlockService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using letterstocrushes.Core.Interfaces;
+using letterstocrushes.Core.Model;
+
+namespace letterstocrushes.Core.Services
+{
+    public class BlockService
+    {
+
+        private readonly IQueryBlocks _queryBlocks;
+        public BlockService(IQueryBlocks queryBlocks)
+        {
+            _queryBlocks = queryBlocks;
+        }
+
+        public void Add(int type, int what, string value)
+        {
+            _queryBlocks.Add(type, value, what);
+        }
+
+        public List<Block> getBlocks(int type, int what)
+        {
+            return _queryBlocks.getBlocks(type, what);
+        }
+
+    }
+}
