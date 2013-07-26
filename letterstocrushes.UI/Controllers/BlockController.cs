fdd7eaecd53010d292657669172713c57c666ac6
Seth Hayward <seth.hayward@gmail.com>
2013-07-26 12:17:40 -0700

    showing a count of IP bans

--- letterstocrushes.UI/Controllers/BlockController.cs
+++ letterstocrushes.UI/Controllers/BlockController.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using letterstocrushes.Core.Services;
 
 namespace letterstocrushes.Controllers
 {
     public class BlockController : Controller
     {
 
+        private readonly BlockService _blockService;
+
+        public BlockController(BlockService blockService)
+        {
+            _blockService = blockService;
+        }
+
         public ActionResult Index()
         {
+
+            ViewBag.IPban = _blockService.getBlocks(Core.Model.blockType.blockIP, Core.Model.blockWhat.blockLetter).Count;
+
             return View();
         }
 
